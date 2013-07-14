52467998ccec973ba68ea37701ce6ffcce9650c8
Pushman <pushman.pl@gmail.com>
2013-07-14 13:17:23 +0200

    Little refactoring in VatCalculator

--- src/main/scala/scala_sugar/functions/VatCalculator.scala
+++ src/main/scala/scala_sugar/functions/VatCalculator.scala
@@ -2,20 +2,23 @@ package scala_sugar.functions
 
 object VatCalculator {
 
-    def mailVat(price: Int) = 0
+    val defaultVat: (Int) => Int =
+        createCalculator(23)
 
-    val defaultVat: (Int) => Int = createCalculator(23)
-
-    val booksVat: (Int) => Int = createCalculator(5)
+    val booksVat: (Int) => Int =
+        createCalculator(5)
 
     private def createCalculator(taxValue: Int)(price: Int) =
         price * taxValue / 100
 
+    def mailVat(price: Int) =
+        0
+
     val vatCalculatorPerProductType: Map[String, (Int) => Int] = Map(
-        "mail" -> mailVat _,
-        "book" -> booksVat,
         "bumblebee" -> {
             (price: Int) => price * 8 / 100
-        }
+        },
+        "book" -> booksVat,
+        "mail" -> mailVat _
     ).withDefaultValue(defaultVat)
 }
