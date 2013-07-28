e30dba67f9c29bd5e2c14883da975c12b81f770a
Pushman <pushman.pl@gmail.com>
2013-07-28 20:40:29 +0200

    Standard library - find and partition examples

--- src/main/scala/scala_sugar/standard_library/BooksOrganizer.scala
+++ src/main/scala/scala_sugar/standard_library/BooksOrganizer.scala
@@ -4,16 +4,20 @@ import scala_sugar.pattern_matching.Book
 
 object BooksOrganizer {
 
-    def authorsWithMoreBooksThan(books: List[Book], limit: Int) = {
+    def authorsWithMoreBooksThan(books: List[Book], limit: Int) =
         for {
-            (author, books) <- books.groupBy(_.author)
-            size = books.size if size >= limit
+            (author, books) <- books.groupBy(_.author) if books.size >= limit
         } yield author
+
+    def authorsWithMoreBooksThan_versionB(books: List[Book], limit: Int) = {
+        val byAuthor: Map[String, List[Book]] = books.groupBy(book => book.author)
+        val byAuthorWithCount: Map[String, Int] = byAuthor.mapValues(book => book.size)
+        byAuthorWithCount.filter(tuple => tuple._2 >= limit).keys
     }
 
-    def authorsWithMoreBooksThan2(books: List[Book], limit: Int) = {
-        val byAuthor: Map[String, List[Book]] = books.groupBy(_.author)
-        val byAuthorWithCount: Map[String, Int] = byAuthor.mapValues(_.size)
-        byAuthorWithCount.filter(_._2 >= limit).keys
-    }
+    def findAuthorOfBook(books: List[Book], searchedTitle: String) =
+        books.find(book => book.title == searchedTitle).map(book => book.author)
+
+    def separateBooksOfAuthor(books: List[Book], searchedAuthor: String): (List[Book], List[Book]) =
+        books.partition(_.author == searchedAuthor)
 }
--- src/test/scala/scala_sugar/standard_library/BooksOrganizerTest.scala
+++ src/test/scala/scala_sugar/standard_library/BooksOrganizerTest.scala
@@ -9,16 +9,28 @@ import scala_sugar.pattern_matching.Book
 @RunWith(classOf[JUnitRunner])
 class BooksOrganizerTest extends FunSuite with ShouldMatchers {
 
-    val MartinFowler = "Martin Fowler"
+    val MartinFowler     = "Martin Fowler"
+    val RefactoringTitle = "Refactoring"
 
-    val publications = List(
-        Book("Andrew Hunt", "The Pragmatic Programmer"),
-        Book(MartinFowler, "Refactoring"),
-        Book(MartinFowler, "Patterns of Enterprise Application Architecture"),
-        Book("Joshua Bloch", "Effective Java")
-    )
+    val PragmaticProgrammer = Book("Andrew Hunt", "The Pragmatic Programmer")
+    val PoEAA               = Book(MartinFowler, "Patterns of Enterprise Application Architecture")
+    val EffectiveJava       = Book("Joshua Bloch", "Effective Java")
+    val Refactoring         = Book(MartinFowler, RefactoringTitle)
+
+    val books = List(PragmaticProgrammer, Refactoring, PoEAA, EffectiveJava)
 
     test("should take only authors who written 2 or more books") {
-        BooksOrganizer.authorsWithMoreBooksThan(publications, 2) should (contain(MartinFowler) and have size 1)
+        BooksOrganizer.authorsWithMoreBooksThan(books, 2) should (contain(MartinFowler) and have size 1)
+    }
+
+    test("find author of book") {
+        BooksOrganizer.findAuthorOfBook(books, RefactoringTitle) should be(Some(MartinFowler))
+    }
+
+    test("separate Fowler's books from others") {
+        val (ofFowler, others) = BooksOrganizer.separateBooksOfAuthor(books, MartinFowler)
+
+        ofFowler should be(List(Refactoring, PoEAA))
+        others should be(List(PragmaticProgrammer, EffectiveJava))
     }
 }
