39c48e654e4928c9b950b37ac1cbf87ea7eb6b24
Pushman <pushman.pl@gmail.com>
2013-11-11 15:25:15 +0100

    Updated .gitgnore

--- src/main/scala/scala_sugar/task/CDLibrary.scala
+++ src/main/scala/scala_sugar/task/CDLibrary.scala
@@ -1,15 +1,18 @@
 package scala_sugar.task
 
 object CDLibrary {
-
-    def calculateMeanMark(albums: List[Album]): Double =
-        ???
+    
+    def calculateMeanMark(albums: List[Album]): Double = 
+        albums.map(_.mark).sum / albums.size.toDouble
 
     def extractVeryGoodSongs(albums: List[Album]): List[Song] =
-        ???
+        extractSongs(albums.filter(_.mark == 5))
 
     def extractSongs(albums: List[Album]): List[Song] =
-        ???
+        for {
+            album <- albums
+            song <- album.songs
+        } yield Song(album.artist, song)
 }
 
 case class Song(artist: String, title: String)
